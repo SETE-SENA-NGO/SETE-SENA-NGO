1d3579ffce5345adc176f13d5e79d50c4eb18d35
keosreydoeurn <YOUR_EMAIL@example.com>
2026-07-06 16:58:32 +0700

    refactor: remove site nav and footer components

--- src/router/publicRoutes.ts
+++ src/router/publicRoutes.ts
@@ -2,10 +2,12 @@ import HomeView from '@/views/public/HomeView.vue'
 import AboutView from '@/views/public/AboutView.vue'
 import ServicesView from '@/views/public/ServicesView.vue'
 import ContactView from '@/views/public/ContactView.vue'
+import ImpactView from '@/views/public/ImpactView.vue'
 
 export const publicRoutes = [
   { path: '/', component: HomeView },
   { path: '/about', component: AboutView },
   { path: '/services', component: ServicesView },
   { path: '/contact', component: ContactView },
+  { path: '/impact', component: ImpactView },
 ]
