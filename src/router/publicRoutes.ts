dffb609f372f8871a3af35d87cc6b1485add1771
SREYMOM.PO <SREYMOM.PO@PNC.LOCAL>
2026-07-07 09:46:05 +0700

    feat: developing code in programs page in point education with content and image, but don't have navbar and footer yet

--- src/router/publicRoutes.ts
+++ src/router/publicRoutes.ts
@@ -2,12 +2,14 @@ import HomeView from '@/views/public/HomeView.vue'
 import AboutView from '@/views/public/AboutView.vue'
 import ServicesView from '@/views/public/ServicesView.vue'
 import ContactView from '@/views/public/ContactView.vue'
-import ProgramsView from '@/views/public/ProgramsView.vue' 
+import ProgramsView from '@/views/public/ProgramsView.vue'
+import ProgramsEducationView from '@/views/public/ProgramsEducationView.vue'  // ← ថ្មី
 
 export const publicRoutes = [
   { path: '/', component: HomeView },
   { path: '/about', component: AboutView },
   { path: '/services', component: ServicesView },
   { path: '/contact', component: ContactView },
-  { path: '/programs', component: ProgramsView }, 
+  { path: '/programs', component: ProgramsView },
+  { path: '/programs/education', component: ProgramsEducationView, name: 'programs-education' },  // ← ថ្មី
 ]
