18787e89972d67541bb38e3fb137080b059a1100
SREYMOM.PO <SREYMOM.PO@PNC.LOCAL>
2026-07-07 14:57:18 +0700

    chore: remove Education and Environment pages and routes to avoid conflict

--- src/router/publicRoutes.ts
+++ src/router/publicRoutes.ts
@@ -3,8 +3,6 @@ import AboutView from '@/views/public/AboutView.vue'
 import ServicesView from '@/views/public/ServicesView.vue'
 import ContactView from '@/views/public/ContactView.vue'
 import ProgramsView from '@/views/public/ProgramsView.vue'
-import ProgramsEducationView from '@/views/public/ProgramsEducationView.vue'
-import ProgramsEnvironmentView from '@/views/public/ProgramsEnvironmentView.vue'
 import ProgramsLivelihoodView from '@/views/public/ProgramsLivelihoodView.vue'
 import ProgramsChildProtectionView from '@/views/public/ProgramsChildProtectionView.vue'
 
@@ -14,8 +12,6 @@ export const publicRoutes = [
   { path: '/services', component: ServicesView },
   { path: '/contact', component: ContactView },
   { path: '/programs', component: ProgramsView, name: 'programs' },
-  { path: '/programs/education', component: ProgramsEducationView, name: 'programs-education' },
-  { path: '/programs/environment', component: ProgramsEnvironmentView, name: 'programs-environment' },
   { path: '/programs/livelihood', component: ProgramsLivelihoodView, name: 'programs-livelihood' },
   { path: '/programs/child-protection', component: ProgramsChildProtectionView, name: 'programs-child-protection' },
 ]
