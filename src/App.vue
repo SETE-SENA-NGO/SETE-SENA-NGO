7e47a169a4e4c649aefb5446638bf06cc06d9ad4
satan8080 <satanbuntit.it@gmail.com>
2026-07-07 10:04:13 +0700

    add feature contect

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,5 @@
 <script setup lang="ts">
-<<<<<<< HEAD
-import HeaderView from '@/layouts/HeaderView.vue'
-=======
 import FooterView from '@/layouts/FooterView.vue'
->>>>>>> a69e5208c3ed4d16e3dc545775e78be3d1aa8918
 </script>
 
 <template>
