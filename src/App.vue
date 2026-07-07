1d6e6554820f3f561a50b5fa084109b2b13b9af3
satan8080 <satanbuntit.it@gmail.com>
2026-07-07 16:28:05 +0700

    fix: updat contacgt page

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,6 @@
 <script setup lang="ts">
-<<<<<<< HEAD
-import HeaderView from '@/layouts/HeaderView.vue'
-import FooterView from '@/layouts/FooterView.vue'
-=======
 import FooterView from '@/layouts/FooterView.vue'
 import HeaderView from '@/layouts/HeaderView.vue'
->>>>>>> 80aef6b07918b6211b72b11bd830c3f88ae3c125
 </script>
 
 <template>
@@ -13,4 +8,3 @@ import HeaderView from '@/layouts/HeaderView.vue'
   <RouterView />
   <FooterView />
 </template>
-
