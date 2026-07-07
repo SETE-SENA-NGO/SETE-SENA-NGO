37f185aa4a902e22224e80440c077bebc028164d
SIENG SOPHAT <youphat45@gmail.com>
2026-07-07 08:01:48 +0700

    feat: create about vision page

--- src/router/publicRoutes.ts
+++ src/router/publicRoutes.ts
@@ -1,11 +1,13 @@
 import HomeView from '@/views/public/HomeView.vue'
 import AboutView from '@/views/public/AboutView.vue'
+import AboutVisionView from '@/views/AboutVisionView.vue'
 import ServicesView from '@/views/public/ServicesView.vue'
 import ContactView from '@/views/public/ContactView.vue'
 
 export const publicRoutes = [
   { path: '/', component: HomeView },
   { path: '/about', component: AboutView },
+  { path: '/about/vision', component: AboutVisionView },
   { path: '/services', component: ServicesView },
   { path: '/contact', component: ContactView },
 ]
