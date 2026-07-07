b799221c9164de8a2c42929df55b87eaf50e75a6
keosreydoeurn <YOUR_EMAIL@example.com>
2026-07-07 16:14:03 +0700

    fix: fix develop

--- src/layouts/HeaderView.vue
+++ src/layouts/HeaderView.vue
@@ -35,7 +35,7 @@ const menus: Menu[] = [
     label: 'Impact',
     to: '/impact',
     items: [
-      { title: 'Overview', desc: '293 villages reached since 1994.', to: '/impact/numbers' },
+      { title: 'Numbers', desc: '293 villages reached since 1994.', to: '/impact/numbers' },
       { title: 'Timeline', desc: 'Milestones from 1994 to 2024.', to: '/impact/timeline' },
       { title: 'Partners', desc: 'UNDP, ADB, Oxfam and more.', to: '/impact/partners' },
     ],
